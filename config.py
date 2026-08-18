a4b6235e2a2648081f3cc58f5406426a49a14738
Spaandy <alexdreay@yahoo.com>
2026-08-18 18:03:44 +0400

    Initial initialisation

---
+++ config.py
@@ -0,0 +1,4 @@
+APP_NAME = "Athena"
+APP_VERSION = "a0.0.1"
+
+AUTHOR = "Spaandy"
---
+++ main.py
@@ -0,0 +1,10 @@
+import config
+
+## Intro: Project Athena. Verson 0.0.1 initialising...
+
+
+print("=======================================")
+print("=== Project " + config.APP_NAME + "                  ===")
+print("=== Version: " + config.APP_VERSION + " initialising..." + " ===")
+print("=== Author: " + config.AUTHOR + "                 ===")
+print("=======================================")
