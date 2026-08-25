5b759e486fb55df0ea457e449e208626183e88f0
Spaandy <alexdreay@yahoo.com>
2026-08-25 20:45:18 +0100

    Initial testing.

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 APP_NAME = "Athena"
 APP_VERSION = "a0.0.1"
+LLM_MODEL = "qwen3:8b"
 
 AUTHOR = "Spaandy"
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import config
+from ollama import chat
+
 
 ## Intro: Project Athena. Verson 0.0.1 initialising...
 
@@ -8,3 +10,28 @@ print("=== Project " + config.APP_NAME + "                  ===")
 print("=== Version: " + config.APP_VERSION + " initialising..." + " ===")
 print("=== Author: " + config.AUTHOR + "                 ===")
 print("=======================================")
+
+running = True
+
+
+
+
+
+while running:
+
+    ## Stage 1: Ask opening question on launch.
+    question = input("What is your question?")
+
+    ## Get input 
+    response = chat(
+    model=config.LLM_MODEL, messages=[
+        {
+            "role": "user",
+            "content": question
+        }
+    ],
+    think=False
+    )
+    print(response.message.content)
+
+
